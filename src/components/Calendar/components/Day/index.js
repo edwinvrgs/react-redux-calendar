50f50c407095c190ffe3d0f16951cba5c78bcbbf
Edwin Vargas <onix.edwin18@gmail.com>
2019-09-30 01:46:44 -0400

    Hotfix

--- src/components/Calendar/components/Day/index.js
+++ src/components/Calendar/components/Day/index.js
@@ -1,48 +1,90 @@
-import React     from 'react'
-import PropTypes from 'prop-types'
+import React, { useState, useMemo, useCallback } from 'react'
+import { createPortal }                          from 'react-dom'
+import PropTypes                                 from 'prop-types'
+import { useSelector }                           from 'react-redux'
+import { format, isSameDay }                     from 'date-fns'
 
 import { getDayNumber }    from '../../../../utils'
-import RemindersList       from '../RemindersList'
 import { useActions }      from '../../../../hooks'
 import { calendarActions } from '../../../../state/ducks/calendar'
 
+import Reminder             from '../Reminder'
+import RemindersList        from '../RemindersList'
+import { remindersActions } from '../../../../state/ducks/reminders'
+
 const Day = props => {
   const {day, isSelected, isToday, isCurrentMonth} = props
+  const reminders = useSelector(({reminders}) => reminders)
+  const {removeAllReminders} = useActions(remindersActions)
   const {selectDay} = useActions(calendarActions)
+  const [on, toggle] = useState(false)
+
+  const remindersInThisDay = useMemo(
+    () => reminders.filter(reminder => isSameDay(day, reminder.date))
+                   .sort((a, b) => format(a.hour, 't') - format(b.hour, 't')),
+    [day, reminders],
+  )
+
+  const onToggle = useCallback(() => toggle(on => !on), [])
+
+  const onClick = useCallback(() => {
+    selectDay(day)
+    onToggle()
+  }, [day, onToggle, selectDay])
 
   return (
-    <div
-      className=""
-      style={ {
-        flex: 1,
-        height: '150px',
-        borderLeft: 'solid 1px #EFEFEF',
-        cursor: 'pointer',
-        transition: 'all .2s',
-        color: isCurrentMonth ? 'black' : 'grey',
-        backgroundColor: isToday
-          ? 'lightgrey'
-          : isSelected
+    <>
+      {
+        remindersInThisDay.length !== 0 &&
+        createPortal(
+          <div className={ `modal ${ on && 'is-active' }` }>
+            <div className="modal-background" onClick={ onToggle } />
+            <div className="modal-content">
+              <div className="notification">
+                {
+                  remindersInThisDay.map(reminder => (
+                    <Reminder key={ reminder.id } { ...reminder } />
+                  ))
+                }
+              </div>
+            </div>
+          </div>,
+          document.getElementById('modal-root'),
+        )
+      }
+      <div
+        className=""
+        style={ {
+          flex: 1,
+          height: '150px',
+          borderLeft: 'solid 1px #EFEFEF',
+          cursor: 'pointer',
+          transition: 'all .2s',
+          color: isCurrentMonth ? 'black' : 'grey',
+          backgroundColor: isToday
             ? 'lightblue'
-            : '',
-      } }
-      onClick={ () => selectDay(day) }
-    >
-      <div className="columns">
-        <div className="column is-2">
-          <div style={ {
-            fontSize: '1rem',
-            alignSelf: 'flex-start',
-            padding: '8px 0 0 8px',
-          } }>
-            { getDayNumber(day) }
+            : isSelected
+              ? 'lightgrey'
+              : '',
+        } }
+        onClick={ onClick }
+      >
+        <div className="columns">
+          <div className="column is-2">
+            <div style={ {
+              fontSize: '1rem',
+              alignSelf: 'flex-start',
+              padding: '8px 0 0 8px',
+            } }>
+              { getDayNumber(day) }
+            </div>
+          </div>
+          <div className="column">
+            <RemindersList day={ day } />
           </div>
         </div>
-        <div className="column">
-          <RemindersList day={ day } />
-        </div>
       </div>
-    </div>
+    </>
   )
 }
 
